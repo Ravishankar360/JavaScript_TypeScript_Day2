21280cc9a67fa8716d05d96ca38b671e491cd081
unknown <cogjava3300@iiht.tech>
2022-10-12 18:43:06 +0530

    test

---
+++ src/myfirst.ts
@@ -0,0 +1,12 @@
+var message:string="Welcome to type script";
+console.log(message);
+
+
+class greeting {
+    greet():void{
+        console.log("hi good evening");
+    }
+}
+
+var ob1= new greeting;
+console.log("Result "+ ob1);
